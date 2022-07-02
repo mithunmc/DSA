1f2b9bd0590ac0ab027b9b1eb3062a24741ed63c
mithun <mithun.mandayamcomar@boeing.com>
2022-07-02 17:24:30 +0530

    Switch Case

---
+++ src/Topic4_SwitchAndFunctions/SwitchCont.java
@@ -0,0 +1,15 @@
+package Topic4_SwitchAndFunctions;
+
+public class SwitchCont {
+    public static void main(String[] args) {
+        int i = 10;
+        
+        switch(i){
+            case 10:
+                System.out.println(10);
+                continue;
+            default:
+                throw new IllegalStateException("Unexpected value: " + i);
+        }
+    }
+}
